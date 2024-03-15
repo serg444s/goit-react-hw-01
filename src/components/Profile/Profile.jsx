96aea7b534478faa93326e837c8d0b9b4d0492c9
serg444s <143083342+serg444s@users.noreply.github.com>
2024-03-15 19:18:39 +0200

    className={profile}

--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -1,24 +1,26 @@
+import css from "./Profile.module.css";
+
 const Profile = ({ name, tag, location, image, stats }) => {
   return (
-    <div>
-      <div>
-        <img src={image} alt={name} />
-        <p>{name}</p>
-        <p>@{tag}</p>
-        <p>{location}</p>
+    <div className={css.container}>
+      <div className={css.wrap}>
+        <img src={image} alt={name} className={css.card} />
+        <p className={css.title}>{name}</p>
+        <p className={css.text}>@{tag}</p>
+        <p className={css.text}>{location}</p>
       </div>
-      <ul>
-        <li>
-          <span>Followers</span>
-          <span>{stats.followers}</span>
+      <ul className={css.stats}>
+        <li className={css.item}>
+          <span className={css.stat}>Followers</span>
+          <span className={css.number}>{stats.followers}</span>
         </li>
-        <li>
-          <span>Views</span>
-          <span>{stats.views}</span>
+        <li className={css.item}>
+          <span className={css.stat}>Views</span>
+          <span className={css.number}>{stats.views}</span>
         </li>
-        <li>
-          <span>Likes</span>
-          <span>{stats.likes}</span>
+        <li className={css.item}>
+          <span className={css.stat}>Likes</span>
+          <span className={css.number}>{stats.likes}</span>
         </li>
       </ul>
     </div>
