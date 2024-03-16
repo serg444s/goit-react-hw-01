c290034af4ba20dff342f9e338480b512a24a765
serg444s <143083342+serg444s@users.noreply.github.com>
2024-03-16 12:29:43 +0200

    json

--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -2,7 +2,7 @@
 // import { HiUser } from "react-icons/hi";
 import css from "./Profile.module.css";
 
-const Profile = ({ name, tag, location, image, stats }) => {
+const Profile = ({ image, name, tag, location, stats }) => {
   return (
     <div className={css.container}>
       <div className={css.wrap}>
